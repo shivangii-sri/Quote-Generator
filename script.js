f449d136debb38f0f7fb1d96c43eaf24df070e87
shivangii-sri <shivangisrivastava762@gmail.com>
2023-11-02 00:07:42 +0530

    Added Loader functionality

--- script.js
+++ script.js
@@ -1,13 +1,29 @@
-const quoteContainer = document.getElementById('quote-conatiner');
+const quoteContainer = document.getElementById('quote-container'); 
 const quoteText = document.getElementById('quote');
 const authorText = document.getElementById('author');
 const twitterBtn = document.getElementById('twitter');
 const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
 
 let apiQuotes = [];
 
+// Show Loading
+function loading(){
+    loader.hidden = false;
+    quoteContainer.hidden = true; 
+}
+
+// Hide Loading
+function complete(){
+    quoteContainer.hidden = false;
+    loader.hidden = true;
+}
+
 // Show New Quote
 function newQuote() {
+
+    loading();
+
     // Pick a random quote from apiQuotes array
     const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
 
@@ -26,12 +42,16 @@ function newQuote() {
     else{
         quoteText.classList.remove('long-quote');       
     }
+
+    // Set quote, Hide Loader
     quoteText.textContent = quote.text;
+    complete();
 
 }
 
 // Get Quotes from API
 async function getQuotes() {
+    loading();
     const apiUrl = 'https://jacintodesign.github.io/quotes-api/data/quotes.json';
     try{
         const response = await fetch(apiUrl);
